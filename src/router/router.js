5624ca7f59c55497c9701a9a0416752b39b64cdb
Tran My Linh <linhninh2@gmail.com>
2022-09-28 17:01:05 +0700

    register,login page done and validate front end

--- src/router/router.js
+++ src/router/router.js
@@ -47,7 +47,16 @@ function router(req, res) {
           return res.end();
         });
         break;
-        
+        case "/register":
+          fs.readFile("./src/views/register.html", "utf-8", function (err, data) {
+            if (err) {
+              console.log(err.message);
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end();
+          });
+          break;  
       default:
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write("<h1>No found page !</h1>");
