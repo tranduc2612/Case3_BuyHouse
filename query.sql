a9fb1ea76bc771b774c2af0ebb8e487b5ffb043f
Duc <mintduc2612@gmail.com>
2022-10-15 15:15:55 +0700

    update db

--- query.sql
+++ query.sql
@@ -78,7 +78,7 @@ insert tUser(email,phone,passwordUR,typeDK,gender,address,cccd,nameUser) values
 ('mintduc2612@gmail.com','0367218700','duc123',1,'nam','Phường Cẩm Sơn,thành phố Cẩm Phả,tỉnh Quảng Ninh','022202002528','Trần Minh Đức'),
 ('Minhanh190202@gmail.com','0978363413','minhanh123',1,'nam','Huyện Gia Lâm,thành phố Hà Nội','001302011234','Trần Minh Anh');
 
-select * from Post;
+
 insert Post(title,userId,datePost,address,lat,lng,cost,statusHouse,descriptionPost)
 values ('Cho thuê nhà ở Láng Hạ, Đống Đa',2,'2022-10-10','Láng Hạ - Đống Đa - Hà Nội','21.016748','105.810718',14000000,'cho thuê','Cho thuê nhà tại phố Hoàng Ngọc Phách, Láng Hạ, quận Đống Đa, Hà Nội 
 - Nhà tầng 1 mặt phố, diện tích 32m2.
@@ -114,7 +114,6 @@ values
 ('Cho thuê mặt bằng, mặt tiền tại Quận 12 ',2,'2018-12-11','Tân Chánh Hiệp - Quận 12 - TPHCM','10.874773','106.622543',25000000,'cho thuê','Cho thuê mặt bằng, mặt tiền 146 Nguyễn Ảnh Thủ, P.Tân Chánh Hiệp, Q12
 Ngang 7x12m, giá 25 triệu');
 
-
 insert Image(postId, url)
 values(1,'https://cloud.muaban.net/images/2022/09/15/525/e2895ab6d9994a4596e41638be0e0f58.jpg'),
 (1,'https://cloud.muaban.net/images/2022/09/15/524/d9a2de32df9d49669f7771f865111d03.jpg'),
@@ -135,9 +134,29 @@ values(2,'https://cloud.muaban.net/images/2022/10/03/594/627f903d015d4e2c870d10c
 (5,'https://cloud.muaban.net/images/2022/09/29/237/0ba927e80cdc4178a653e4aad15b3bcf.jpg'),
 (5,'https://cloud.muaban.net/images/2022/09/29/236/3f4feb0461e348779f15a57aee1c680d.jpg'),
 (5,'https://cloud.muaban.net/images/2022/09/29/237/de55134ba6974bfea8848191984ce0c3.jpg');
+insert Image(postId, url)
+values 
+(6,'https://cloud.muaban.net/images/2022/10/10/572/376a4e6b234042b49c8a0c01e5d2661c.jpg'),
+(6,'https://cloud.muaban.net/images/2022/10/10/572/2248423affd649a79e25a425f7947401.jpg'),
+(6,'https://cloud.muaban.net/images/2022/10/10/572/5445d8fb46ad4289a043c4e373d8cba7.jpg'),
+(6,'https://cloud.muaban.net/images/2022/10/10/572/787974b4f0aa47b582973d46181d9c71.jpg'),
+(7,'https://cloud.muaban.net/images/2022/08/15/103/e1551e592d0e4f19a9e651cfe5d8c9e3.jpg'),
+(7,'https://cloud.muaban.net/images/2022/08/15/104/0f4ced6fe78f44a9bd8021dab0654b88.jpg'),
+(7,'https://cloud.muaban.net/images/2022/08/15/109/62608136d30a442c996c4edfa0e5d8b6.jpg'),
+(7,'https://cloud.muaban.net/images/2022/08/15/104/1858eaa5954c4fef8b8ba1ea9d1147fd.jpg'),
+(7,'https://cloud.muaban.net/images/2022/08/15/102/aa52a22a99bf45a7859c2f7a5b75fc1a.jpg'),
+(8,'https://cloud.muaban.net/images/2022/10/14/247/6de7836f5cfc4557bbf8695f9b1e28aa.jpg'),
+(8,'https://cloud.muaban.net/images/2022/10/14/247/7fc7ded222064caca33260412c85a405.jpg'),
+(9,'https://cloud.muaban.net/images/2022/10/13/221/9935b693180c4743b607dcb14bfd8444.jpg'),
+(9,'https://cloud.muaban.net/images/2022/10/13/223/d340b4a7b5ff4b6d87ad6ee2e94b55d3.jpg'),
+(9,'https://cloud.muaban.net/images/2022/10/13/221/d83ce7f74ccd4ac6aa7347d1c899cd9b.jpg'),
+(9,'https://cloud.muaban.net/images/2022/10/13/221/f4383989522641319595007811d75af5.jpg');
 
-select * from tUser;
-select * from Post;
-select * from Post join Image on Post.postId = Image.postId group by Post.postId
 
+select  * from Image;
 
+select distinct Post.postId, title, cost, datePost,address, url from Post join Image on Image.postId = Post.postId group by Post.postId;
+
+update tUser 
+set passwordUR = 'Linh123'
+where userId = 1
