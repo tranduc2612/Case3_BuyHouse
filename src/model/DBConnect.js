a8244546b36649de76156109e0ad5c7fd730493c
Duc <mintduc2612@gmail.com>
2023-02-23 12:19:10 +0700

    db deploy

--- src/model/DBConnect.js
+++ src/model/DBConnect.js
@@ -2,26 +2,35 @@ const mysql = require("mysql");
 const fs = require("fs");
 
 class DBConnection {
-  constructor() {
-    this.connection = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "12345678",
-      database: "BuyHouse",
-      charset: "utf8_general_ci",
-    });
-  }
+	constructor() {
+		// this.connection = mysql.createConnection({
+		//   host: "localhost",
+		//   user: "root",
+		//   password: "12345678",
+		//   database: "BuyHouse",
+		//   charset: "utf8_general_ci",
+		// });
 
-  async runMySQL(sql) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(sql, (err, results, fields) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(results);
-      });
-    });
-  }
+		this.connection = mysql.createConnection({
+			host: "containers-us-west-114.railway.app",
+			user: "root",
+			password: "OEjAkBkEI6KCf5SdouDi",
+			database: "railway",
+			charset: "utf8_general_ci",
+		});
+	}
+
+	async runMySQL(sql) {
+		return new Promise((resolve, reject) => {
+			this.connection.query(sql, (err, results, fields) => {
+				if (err) {
+					reject(err);
+				}
+				console.log("success");
+				resolve(results);
+			});
+		});
+	}
 }
 
 module.exports = new DBConnection();
