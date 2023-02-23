ec1283581048bdd79bae5572fe7b7b05ff5b60bd
Duc <mintduc2612@gmail.com>
2023-02-23 14:29:26 +0700

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const qs = require("qs");
 require("dotenv").config();
 const router = require("./src/router/router.js");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(router);
 server.listen(PORT, () => {
